f8b689f36093453b2cf4a338044229386ab17f28
nmcanall <nmcanall@citadel.edu>
2020-06-08 21:20:12 -0700

    updated form submit functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,22 @@ var createTaskHandler = function(event) {
 
     event.preventDefault();
 
+    // Get the form input
+    var taskNameInput = document.querySelector("input[name='task-name'").value;
+    var taskTypeInput = document.querySelector("select[name='task-type'").value;
+
+    // Add new list item
     var taskItemEl = document.createElement("li");
     taskItemEl.className = "task-item";
-    taskItemEl.textContent = "This is your new task.";
+
+    // Create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    
+
+    // Add new list item to the list
+    taskItemEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(taskItemEl);
 }
 
