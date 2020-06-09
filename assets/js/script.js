3d476f44e328c1537ac066f2fdcf7d239aac9bbb
nmcanall <nmcanall@citadel.edu>
2020-06-08 20:56:11 -0700

    updated form submit functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 // Global variables
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // Create new task helper method
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+
     var taskItemEl = document.createElement("li");
     taskItemEl.className = "task-item";
     taskItemEl.textContent = "This is your new task.";
@@ -11,4 +14,4 @@ var createTaskHandler = function() {
 }
 
 // Add a new item to the To-Do List by clicking the button
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
